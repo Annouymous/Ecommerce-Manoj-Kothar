4fc4adf2df40494ca9d6fd2a27c1eaca7b2cebd8
Annouymous <arpitarya426@gmail.com>
2024-11-30 09:35:44 +0530

    Api Correction

--- Firebase/AdminConfig.tsx
+++ Firebase/AdminConfig.tsx
@@ -1,13 +1,13 @@
-import admin from 'firebase-admin';
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert({
-            clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-            privateKey:process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
-            projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-        }),
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    }),
+  });
 }
 
 export const AdminAuth = admin.auth();
